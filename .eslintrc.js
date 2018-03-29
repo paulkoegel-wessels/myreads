3344af15b91ec9f0601b0f181f71dba7562b4e4a
Paul Kögel <p.koegel@wessels-technologie.de>
2018-03-29 11:15:19 +0200

    chore: ESLint and editor config setup.
    + Add .editorconfig and .eslintrc.js
    + `yarn add -D eslint eslint-config-semistandard-react
      eslint-plugin-semistandard-react`

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  extends: "semistandard-react",
+  rules: {
+    "no-debugger": "off",
+    "no-useless-return": "off",
+    quotes: ["error", "single"],
+    "semistandard-react/prop-types": "off",
+    "space-before-function-paren": "error",
+    "space-infix-ops": "off"
+  }
+};
