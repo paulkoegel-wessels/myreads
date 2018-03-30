32048c3694e08f9f05d8620e46d80c20b7c39d22
Paul Kögel <p.koegel@wessels-technologie.de>
2018-03-30 22:55:16 +0200

    chore: add babel-eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,18 @@
 module.exports = {
-  extends: "semistandard-react",
+  parser: 'babel-eslint',
+  extends: 'semistandard-react',
+  env: {
+    browser: true,
+    jest: true,
+  },
   rules: {
-    "no-debugger": "off",
-    "no-useless-return": "off",
-    quotes: ["error", "single"],
-    "semistandard-react/prop-types": "off",
-    "space-before-function-paren": "error",
-    "space-infix-ops": "off"
+    codeFrame: false, // recommended by babel-eslint
+    'no-debugger': 'off',
+    'no-useless-return': 'off',
+    quotes: ['error', 'single'],
+    'semistandard-react/prop-types': 'off',
+    'space-before-function-paren': 'error',
+    'space-infix-ops': 'off',
+    strict: 0 // recommended by babel-eslint
   }
 };
