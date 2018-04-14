0263f2ec1a1a0faecff2f84f0ecccb0fc968d39f
Paul Kögel <p.koegel@wessels-technologie.de>
2018-04-14 20:02:51 +0200

    fix: Book: some books have no thumbnail (e.g. when searching for "biography").

--- src/Book.js
+++ src/Book.js
@@ -7,7 +7,7 @@ export default class Book extends React.Component {
       <li>
         <div className='book'>
           <div className='book-top'>
-            <div className='book-cover' style={{ width: 128, height: 193, backgroundImage: `url("${book.imageLinks.thumbnail})` }} />
+            <div className='book-cover' style={{ width: 128, height: 193, backgroundImage: `url("${book.imageLinks && book.imageLinks.thumbnail})` }} />
             <div className='book-shelf-changer'>
               <select onChange={event => onShelfChange(book.id, event.target.value)} value={book.shelf || 'none'}>
                 <option value='none' disabled>Move to...</option>
